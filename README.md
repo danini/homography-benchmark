9ebf2373e0710ce01474ddabca6bd4db0a62c23e
danini <majti89@gmail.com>
2023-03-30 15:54:49 +0000

    New estimators.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ The dataset is available at [link](https://polybox.ethz.ch/index.php/s/R5sPelZ86
 
 # Running homography estimation with OpenCV
 
-The tests runnin OpenCV can be started by calling
+The tests running OpenCV can be started by calling
 ```
 python test_opencv.py --path=<path to the database folder>
 ```
@@ -23,8 +23,74 @@ The following additional parameters are accepted:
 --confidence - The RANSAC confidence. Default: 0.99
 --inlier_threshold - The inlier-outlier threshold in pixels. Default: 15.0
 --maximum_iterations - The maximum number of RANSAC iterations. Default: 1000
---core_number - The core number for the parallel processing
---opencv_flag = { RANSAC, LMEDS, RHO, USAC_MAGSAC } - The flag to select the robust estimator from OpenCV
+--core_number - The core number for the parallel processing. Default: 4
+--opencv_flag = { RANSAC, LMEDS, RHO, USAC_MAGSAC } - The flag to select the robust estimator from OpenCV. Default: RANSAC
+```
+
+# Running homography estimation with MAGSAC++
+
+To build and install `MAGSAC`/`MAGSAC++`, clone the repository as:
+```shell
+$ git clone https://github.com/danini/magsac --recursive
+```
+
+Then it has to be installed by calling
+```bash 
+$ pip3 install -e .
+```
+
+The tests running MAGSAC++ can be started by calling
+```
+python test_magsac.py --path=<path to the database folder>
+```
+
+The following additional parameters are accepted:
+```
+--split = { train, test } - Select the split to be used. Default: train
+--scene = { all, NYC_Library, Alamo, Yorkminster, Tower_of_London, Madrid_Metropolis, Ellis_Island, Roman_Forum, Vienna_Cathedral, Piazza_del_Popolo, Union_Square } - Select the scene. Default: all
+--config_path - The path to the config file
+--snn_threshold - SNN ratio threshold for SIFT correspondence filtering. Default: 0.80
+--confidence - The RANSAC confidence. Default: 0.99
+--inlier_threshold - The inlier-outlier threshold in pixels. Default: 15.0
+--minimum_iterations - The maximum number of RANSAC iterations. Default: 50
+--maximum_iterations - The maximum number of RANSAC iterations. Default: 1000
+--use_magsac_plus_plus = { 0, 1 } - A boolean deciding if MAGSAC++ or MAGSAC should be used.
+--sampler = { Uniform, PROSAC, PNAPSAC, Importance, ARSampler  } - The sampler used: Uniform, PROSAC [1], PNAPSAC [2], Importance [3], ARSampler [4]. Importance and ARSampler are initialized with the SNN ratios as inlier probabilities. Default: PROSAC
+--core_number - The core number for the parallel processing. Default: 4
+```
+
+# Running homography estimation with Graph-Cut RANSAC
+
+To build and install `GC-RANSAC`, clone the repository as:
+```shell
+$ git clone https://github.com/danini/graph-cut-ransac
+```
+
+Then it has to be installed by calling
+```bash 
+$ pip3 install -e .
+```
+
+The tests running GC-RANSAC can be started by calling
+```
+python test_gcransac.py --path=<path to the database folder>
+```
+
+The following additional parameters are accepted:
+```
+--split = { train, test } - Select the split to be used. Default: train
+--scene = { all, NYC_Library, Alamo, Yorkminster, Tower_of_London, Madrid_Metropolis, Ellis_Island, Roman_Forum, Vienna_Cathedral, Piazza_del_Popolo, Union_Square } - Select the scene. Default: all
+--config_path - The path to the config file
+--snn_threshold - SNN ratio threshold for SIFT correspondence filtering. Default: 0.80
+--confidence - The RANSAC confidence. Default: 0.99
+--inlier_threshold - The inlier-outlier threshold in pixels. Default: 15.0
+--minimum_iterations - The maximum number of RANSAC iterations. Default: 50
+--maximum_iterations - The maximum number of RANSAC iterations. Default: 1000
+--spatial_coherence_weight = A boolean deciding if MAGSAC++ or MAGSAC should be used.
+--neighborhood_size = The radius of the hyper-sphere determining the neighborhood. Default: 20 px
+--sampler = { Uniform, PROSAC, PNAPSAC, Importance, ARSampler  } - The sampler used: Uniform, PROSAC [1], PNAPSAC [2], Importance [3], ARSampler [4]. Importance and ARSampler are initialized with the SNN ratios as inlier probabilities. Default: PROSAC
+--solver = { point, sift, affine } - The minimal solver used for homography estimation. "point": 4PC algorithm, "sift": 2SIFT estimator [5], "affine": 2AC estimator [6].
+--core_number - The core number for the parallel processing. Default: 4
 ```
 
 # Benchmarking results
@@ -50,3 +116,17 @@ When using the dataset, please cite
 }
 
 ```
+
+## References
+<a id="1">[1]</a> 
+Chum, Ondrej, and Jiri Matas. "Matching with PROSAC-progressive sample consensus." 2005 IEEE computer society conference on computer vision and pattern recognition (CVPR'05). Vol. 1. IEEE, 2005.
+<a id="2">[2]</a> 
+Barath, Daniel, et al. "MAGSAC++, a fast, reliable and accurate robust estimator." Proceedings of the IEEE/CVF conference on computer vision and pattern recognition. 2020.
+<a id="3">[3]</a> 
+Brachmann, Eric, and Carsten Rother. "Neural-guided RANSAC: Learning where to sample model hypotheses." Proceedings of the IEEE/CVF International Conference on Computer Vision. 2019.
+<a id="4">[4]</a> 
+Tong, Wei, Jiri Matas, and Daniel Barath. "Deep magsac++." arXiv preprint arXiv:2111.14093 (2021).
+<a id="5">[5]</a> 
+Barath, Daniel, and Zuzana Kukelova. "Homography from two orientation-and scale-covariant features." Proceedings of the IEEE/CVF International Conference on Computer Vision. 2019.
+<a id="6">[6]</a> 
+Barath, Daniel, and Levente Hajder. "A theory of point-wise homography estimation." Pattern Recognition Letters 94 (2017): 7-14.
---
+++ test_magsac.py
@@ -0,0 +1,211 @@
+import cv2
+import h5py
+import os
+import numpy as np
+import argparse
+import yaml
+import multiprocessing
+import time
+from database import load_h5
+from joblib import Parallel, delayed
+from tqdm import tqdm
+from errors import reprojection_error, homography_pose_error, calc_mAA, calc_mAA_pose, homography_pose_error, reprojection_error
+import pymagsac
+
+def run_magsac(pair, scene_scale, matches, relative_pose, image_size1, image_size2, K1, K2, args):
+    # Initialize the errors
+    error = 1e10
+    rotation_error = 1e10
+    translation_error = 1e10
+    absolute_translation_error = 1e10
+    
+    # The SNN ratio
+    snn_ratio = matches[:, 8]
+    # A flag determining whether the point is GT inlier or not.
+    is_inlier_gt = matches[:,9].astype(bool)
+    
+    # Filter by the SNN ratio threshold
+    snn_mask = snn_ratio < args.snn_threshold
+    # The point correspondences
+    correspondences = matches[snn_mask, :4].astype(np.float64)
+    # SIFT angles in the source image
+    angle1 = matches[snn_mask, 4]
+    # SIFT angles in the destinstion image
+    angle2 = matches[snn_mask, 5]
+    # SIFT scale in the source image
+    scale1 = matches[snn_mask, 6]
+    # SIFT scale in the destination image
+    scale2 = matches[snn_mask, 7]
+    # Inlier probabilities
+    probabilities = []
+
+    # Return if there are fewer than 4 correspondences
+    if correspondences.shape[0] < 4:
+        return 0, error, rotation_error, translation_error, 0, absolute_translation_error
+
+    # If the sampler is something else than uniform sampler, the points should be ordered by SNN ratio
+    if args.sampler > 0:
+        snn_ratio = snn_ratio[snn_mask]
+        indices = np.argsort(snn_ratio)
+        snn_ratio = snn_ratio[indices]
+        correspondences = correspondences[indices, :]
+        
+        # If Importance sampler or ARSampler is used, the SNN ratios are converted as probabilities
+        if args.sampler == 3 or args.sampler == 4:
+            point_number = correspondences.shape[0]
+            for i in range(point_number):
+                probabilities.append(1.0 - i / point_number)
+
+    # Run the homography estimation implemented in OpenCV
+    tic = time.perf_counter()
+    H_est, inliers = pymagsac.findHomography(
+        np.ascontiguousarray(correspondences), 
+        image_size1[0][0], image_size1[1][0], 
+        image_size2[0][0], image_size2[1][0],
+        probabilities = probabilities,
+        conf = args.confidence,
+        sampler = args.sampler,
+        min_iters = args.minimum_iterations,
+        max_iters = args.maximum_iterations,
+        use_magsac_plus_plus = args.use_magsac_plus_plus,
+        sigma_th = args.inlier_threshold)
+    toc = time.perf_counter()
+    runtime = toc - tic
+
+    # Count the inliers
+    inlier_number = inliers.sum()
+
+    # Calculate the error if enough inliers are found
+    if inlier_number >= 4:  
+        # The original matches without SNN filtering
+        all_x1y1 = matches[:, :2]
+        all_x2y2 = matches[:, 2:4]
+        # Calculate the re-projection error of the estimated homography given the ground truth inliers
+        error = reprojection_error(all_x1y1[is_inlier_gt], all_x2y2[is_inlier_gt], H_est) if is_inlier_gt.sum() > 1 else 1e10
+        # Calculate the pose error of the estimated homography given the ground truth relative pose
+        rotation_error, translation_error, absolute_translation_error = homography_pose_error(H_est, scene_scale, relative_pose, K1, K2)
+
+    return runtime, error, rotation_error, translation_error, inlier_number, absolute_translation_error
+
+def estimate_homographies(pairs, data, scene_scale, args):
+    assert args.snn_threshold > 0
+    assert args.inlier_threshold > 0
+    assert args.maximum_iterations > 0
+    
+    # Initialize the arrays where the results will be stored
+    times = {}
+    errors = {}
+    rotation_errors = {}
+    translation_errors = {}
+    inlier_numbers = {}
+    absolute_translation_errors = {}
+
+    # Run homography estimation on all image pairs
+    keys = range(len(pairs))
+    results = Parallel(n_jobs=min(args.core_number, len(pairs)))(delayed(run_magsac)(
+        pairs[k], # Name of the current pair
+        scene_scale, # The ground truth scene scale
+        data[f'corr_{pairs[k]}'], # The SIFT correspondences
+        data[f'pose_{pairs[k]}'], # The ground truth relative pose coming from the COLMAP reconstruction
+        data[f"size_{ '_'.join(pairs[k].split('_')[0:3]) }"], # The size of the source image
+        data[f"size_{ '_'.join(pairs[k].split('_')[3:6]) }"], # The size of the destination image
+        data[f"K_{ '_'.join(pairs[k].split('_')[0:3]) }"], # The intrinsic matrix of the source image
+        data[f"K_{ '_'.join(pairs[k].split('_')[3:6]) }"], # The intrinsic matrix of the destination image
+        args) for k in tqdm(keys)) # Other parameters
+
+    for i, k in enumerate(keys):
+        times[k] = results[i][0]
+        errors[k] = results[i][1]
+        rotation_errors[k] = results[i][2]
+        translation_errors[k] = results[i][3]
+        inlier_numbers[k] = results[i][4]
+        absolute_translation_errors[k] = results[i][5]
+    return times, errors, rotation_errors, translation_errors, inlier_numbers, absolute_translation_errors
+
+def sampler_flag(name):
+    if name == "Uniform":
+        return 0
+    if name == "PROSAC":
+        return 1
+    if name == "PNAPSAC":
+        return 2
+    if name == "Importance":
+        return 3
+    if name == "ARSampler":
+        return 4
+    return 0
+
+if __name__ == "__main__":
+    # Passing the arguments
+    parser = argparse.ArgumentParser(description="Running on the HEB benchmark")
+    parser.add_argument('--path', type=str, help="The path to the dataset. It should contain two folders: 'test' and 'train'", default="/")
+    parser.add_argument('--split', type=str, help='Choose a split: train, test', default='train', choices=['test', 'train'])
+    parser.add_argument('--scene', type=str, help='Choose a scene.', default='all', choices=['all', 'NYC_Library', 'Alamo', 'Yorkminster', 'Tower_of_London', 'Madrid_Metropolis', 'Ellis_Island', 'Roman_Forum', 'Vienna_Cathedral', 'Piazza_del_Popolo', 'Union_Square'])
+    parser.add_argument("--config_path", type=str, default='dataset_configuration.yaml')
+    parser.add_argument("--snn_threshold", type=float, default=0.80)
+    parser.add_argument("--confidence", type=float, default=0.99)
+    parser.add_argument("--inlier_threshold", type=float, default=15.0)
+    parser.add_argument("--minimum_iterations", type=int, default=50)
+    parser.add_argument("--maximum_iterations", type=int, default=1000)
+    parser.add_argument("--sampler", type=str, help="Choose from: Uniform, PROSAC, PNAPSAC, Importance, ARSampler.", choices=["Uniform", "PROSAC", "PNAPSAC", "Importance", "ARSampler"], default="PROSAC")
+    parser.add_argument("--use_magsac_plus_plus", type=bool, default=True)
+    parser.add_argument("--core_number", type=int, default=4)
+
+    args = parser.parse_args()
+    
+    split = args.split.upper()
+    if args.use_magsac_plus_plus:
+        print(f"Running MAGSAC++ on the '{split}' split of HEB")
+    else:
+        print(f"Running MAGSAC on the '{split}' split of HEB")
+    args.sampler = sampler_flag(args.sampler)
+
+    # Loading the configuration file
+    with open(args.config_path, "r") as stream:
+        try:
+            configuration = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            exit()
+            
+    # Iterating through the scenes
+    for scene in configuration[f'{split}_SCENES']:
+        # Check if the method should run on a single scene
+        if args.scene != "all" and scene['name'] != args.scene:
+            continue
+        
+        print(100 * "-")
+        print(f"Loading scene '{scene['name']}'")
+        print(100 * "-")
+        
+        # Getting the ground truth reconstruction scale that transform it to metric reconstruction.
+        scale = scene['scale']
+        print(f"The scene scale is {scale}")
+    
+        # Loading the dataset
+        input_fname = os.path.join(args.path, args.split, scene['filename'])
+        data = load_h5(input_fname)
+        pairs = sorted([x.replace('corr_','') for x in data.keys() if x.startswith('corr_')])
+        print(f"{len(pairs)} image pairs are loaded.")
+        
+        # Run homography estimation on the entire scene
+        times, errors, rotation_errors, translation_errors, inlier_numbers, absolute_translation_errors = estimate_homographies(pairs, data, scale, args)
+        
+        # Calculating the pose error as the maximum of the rotation and translation errors
+        maximum_pose_errors = {}
+        for i in range(len(pairs)):
+            maximum_pose_errors[i] = max(rotation_errors[i], translation_errors[i])
+
+        # Calculating the mean Average Accuracy
+        mAA_repr = calc_mAA(errors)
+        mAA_max_pose_error = calc_mAA_pose(maximum_pose_errors)
+        mAA_rotation = calc_mAA_pose(rotation_errors)
+        mAA_abs_translation = calc_mAA_pose(absolute_translation_errors, ths=np.linspace(0.1, 5, 10))
+        
+        print(f"mAA re-projection error = {mAA_repr:0.4f}")
+        print(f"mAA angular pose error = {mAA_max_pose_error:0.4f}")
+        print(f"mAA rotation error = {mAA_rotation:0.4f}")
+        print(f"mAA abs. translation error = {mAA_abs_translation:0.4f}")
+        print(f"Avg. inlier number = {np.mean(list(inlier_numbers.values())):0.1f}")
+        print(f"Avg. run-time = {np.mean(list(times.values())):0.4f} secs")
+        
